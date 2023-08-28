a1ab1a7bb15679e6bdd29c62bc3a06747299a17a
Jaero0 <wjdwogh1224@gmail.com>
2023-08-28 13:05:14 +0900

    필수사항 2번까지 완료

--- TeamRPG/TeamRPG/TeamRPG.cs
+++ TeamRPG/TeamRPG/TeamRPG.cs
@@ -1,12 +1,58 @@
 ﻿using System;
+using TeamRPG;
 
-namespace TexmRPG
+namespace TeamRPG
 {
-    class Program
+    internal class Program
     {
+        private static Character player;
+
         static void Main(string[] args)
         {
+            GameDataSetting();
+            DisplayGameIntro();
+        }
 
+        static void GameDataSetting()
+        {
+            // 캐릭터 정보 세팅
+            player = new Character("Chad", "전사", 1, 10, 5, 100, 1500);
+
+            // 아이템 정보 세팅
+        }
+
+        static void DisplayGameIntro()
+        {
+            Console.Clear();
+
+            Console.WriteLine("스파르타 마을에 오신 여러분 환영합니다.");
+            Console.WriteLine("이곳에서 전전으로 들어가기 전 활동을 할 수 있습니다.");
+            Console.WriteLine();
+            Console.WriteLine("1. 상태보기");
+            Console.WriteLine("2. 인벤토리");
+            Console.WriteLine();
+            Console.WriteLine("원하시는 행동을 입력해주세요.");
+
+            int input = Utility.CheckValidInput(1, 2);
+        }
+
+        static void DisplayMyInfo()
+        {
+            Console.Clear();
+
+            Console.WriteLine("상태보기");
+            Console.WriteLine("캐릭터의 정보르 표시합니다.");
+            Console.WriteLine();
+            Console.WriteLine($"Lv.{player.Level}");
+            Console.WriteLine($"{player.Name}({player.Job})");
+            Console.WriteLine($"공격력 :{player.Atk}");
+            Console.WriteLine($"방어력 : {player.Def}");
+            Console.WriteLine($"체력 : {player.Hp}");
+            Console.WriteLine($"Gold : {player.Gold} G");
+            Console.WriteLine();
+            Console.WriteLine("0. 나가기");
+
+            int input = Utility.CheckValidInput(0, 0);
         }
     }
 }
---
+++ TeamRPG/TeamRPG/Utility.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TexmRPG
+{
+    public class Utility
+    {
+        public static int CheckValidInput(int min, int max)
+        {
+            while (true)
+            {
+                string input = Console.ReadLine();
+
+                bool parseSuccess = int.TryParse(input, out var ret);
+                if (parseSuccess)
+                {
+                    if (ret >= min && ret <= max)
+                        return ret;
+                }
+
+                Console.WriteLine("잘못된 입력입니다.");
+            }
+        }
+    }
+}
