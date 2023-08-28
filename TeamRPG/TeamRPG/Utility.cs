8251709dfae553daf4f239417b1b14e74d8a8b50
Munch310 <ghkdlsi@naver.com>
2023-08-28 14:40:48 +0900

    문현우/Update Utility.cs
    - try catch 구문 수정.
    
    1. 오류 메시지가 출력되지 않았던 버그 수정.

--- TeamRPG/TeamRPG/Utility.cs
+++ TeamRPG/TeamRPG/Utility.cs
@@ -31,18 +31,26 @@ namespace TeamRPG
                 string input = Console.ReadLine();
                 try
                 {
+                    //------문현우 try catch----------
+                    // 입력된 문자열을 정수로 변환 -> t, 정수 값 -> ret
+                    // 실패 -> f , FormatException 발생.
                     bool parseSuccess = int.TryParse(input, out var ret);
-                    if (parseSuccess)
+                    if (!parseSuccess)
                     {
-                        return ret;
+                        //throw new FormatException();
+                        //throw new OverflowException();
+                        throw new FormatException("알맞은 숫자를 다시 입력하세요.");
                     }
+                    return ret;
                 }
-                catch (Exception err)
+                catch (FormatException ex)
                 {
-                    Console.WriteLine("알맞은 숫자를 다시 입력하세요");
-                    Console.ReadKey();
+                    // try에서 발생한 오류를 catch 블록에서 처리.
+                    Console.WriteLine("알맞은 숫자가 아닙니다.\n" + ex.Message);
+                    //-------------------------------------
                 }
             }
+
         }
     }
 }
