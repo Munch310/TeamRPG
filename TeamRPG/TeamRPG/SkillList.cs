911e5b7a4bd57a20484511169416bf873eb94288
audrms6494 <84899482+audrms6494@users.noreply.github.com>
2023-08-29 13:37:29 +0900

    Update SkillList.cs

--- TeamRPG/TeamRPG/SkillList.cs
+++ TeamRPG/TeamRPG/SkillList.cs
@@ -10,9 +10,13 @@ namespace TeamRPG
     // 스킬 목록 구현
     public class SkillList
     {
+        // 전사
         // 알파 스트라이크
         Skill alphaStrike = new Skill("알파 스트라이크", "공격력 * 2로 하나의 적을 공격합니다.", 10);
         // 더블 스트라이크
         Skill doubleStrike = new Skill("더블 스트라이크", "공격력 * 1.5로 2명의 적을 랜덤으로 공격합니다.", 15);
+
+        // 마법사
+        //
     }
 }
