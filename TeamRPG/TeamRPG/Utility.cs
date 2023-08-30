36c82dbe5bc28000e91956798f29c58b707d4563
Jaero0 <124887853+Jaero0@users.noreply.github.com>
2023-08-30 13:44:13 +0900

    Revert "Song"

--- TeamRPG/TeamRPG/Utility.cs
+++ TeamRPG/TeamRPG/Utility.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+//using System.Diagnostics;
+//using System.Reflection;
 using TeamRPG;
 
 namespace TeamRPG
@@ -23,15 +25,21 @@ namespace TeamRPG
                     bool parseSuccess = int.TryParse(input, out var ret);
                     if (!parseSuccess)
                     {
-                        throw new FormatException("숫자를 입력해주세요: ");
+                        // throw new FormatException();
+                        // throw new OverflowException();
+                        throw new FormatException("알맞은 숫자를 다시 입력하세요.");
                     }
                     return ret;
                 }
                 catch (FormatException ex)
                 {
+                    // 현재 메서드를 불러오려 했으나, CheckValidInput 호출.
+                    // string currentMethod = MethodBase.GetCurrentMethod().Name;
+                    // Console.WriteLine(currentMethod);
+
                     // try에서 발생한 오류를 catch 블록에서 처리.
-                    Console.SetCursorPosition(3, 27);
-                    Console.Write(ex.Message);
+                    
+                    Console.WriteLine("알맞은 숫자가 아닙니다.\n" + ex.Message);
                     // break;
                     // -------------------------------------
                 }
